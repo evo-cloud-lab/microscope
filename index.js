7a98375b58957d4354d29aa1cde92c156cf04d49
EaseWay Hu <easeway@gmail.com>
2013-05-28 23:21:40 +0800

    add Collector framework

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Collector:      require('./lib/Collector'),
+    MuxCollector:   require('./lib/MuxCollector'),
+    DeMuxCollector: require('./lib/DeMuxCollector'),
+    Filter:         require('./lib/Filter'),
+    NetCollector:   require('./lib/NetCollector'),
+    LineSplitter:   require('./lib/LineSplitter')
+};
